62fe1d21938c3fe391221f03becad7dad9718265
Максим Кирсанов <kirsmaxi@gmail.com>
2024-01-11 18:19:54 +0300

    upd8

--- getFilms.js
+++ getFilms.js
@@ -2,22 +2,27 @@ import axios from "axios";
 import cheerio from "cheerio";
 
 export const getFilms = async (text) => {
-  const films = [];
-  const response = await axios.get(
-    `https://b.kinozadrot5.cc/index.php?do=search&subaction=search&titleonly=3&story=${text}`
-  );
-  const $ = cheerio.load(response.data);
-  $(".th-item").each((index, element) => {
-    const href = $(element).find("a").attr("href");
-    const title = $(element).find(".th-title").text();
-    const img =
-      "https://b.kinozadrot5.cc/" + $(element).find(".th-img img").attr("src");
-    const film = {
-      href,
-      title,
-      img,
-    };
-    films.push(film);
-  });
-  return films;
+  try {
+    const films = [];
+    const response = await axios.get(
+      `https://b.kinozadrot5.cc/index.php?do=search&subaction=search&titleonly=3&story=${text}`
+    );
+    const $ = cheerio.load(response.data);
+    $(".th-item").each((index, element) => {
+      const href = $(element).find("a").attr("href");
+      const title = $(element).find(".th-title").text();
+      const img =
+        "https://b.kinozadrot5.cc/" +
+        $(element).find(".th-img img").attr("src");
+      const film = {
+        href,
+        title,
+        img,
+      };
+      films.push(film);
+    });
+    return films;
+  } catch (error) {
+    console.log(error);
+  }
 };
